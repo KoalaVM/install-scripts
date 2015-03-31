588b6665c403364a5433cf8e273e083b26ad54fd
Clay Freeman <git@clayfreeman.com>
2015-03-30 19:11:12 -0500

    Revert d093284..9748927
    This rolls back to commit d0932842c4078020794f46b42fc180f5518a2c57.

--- install-node.sh
+++ install-node.sh
@@ -6,13 +6,13 @@ if [ "`whoami`" != "root" ]; then
 fi
 
 # Setup environment variables
-export   confdphp="/etc/php5/conf.d"
-export      crond="/etc/cron.d"
-export     koalad="/usr/local/koalad"
-export libvirtphp="http://libvirt.org/sources/php/libvirt-php-0.4.8.tar.gz"
-export     logdir="/var/log"
-export         wd="`pwd`"
-export        tmp="/tmp"
+export    confdphp="/etc/php5/conf.d"
+export       crond="/etc/cron.d"
+export      koalad="/usr/local/koalad"
+export  libvirtphp="http://libvirt.org/sources/php/libvirt-php-0.4.8.tar.gz"
+export      logdir="/var/log"
+export          wd="`pwd`"
+export         tmp="/tmp"
 
 # Upgrade the current system
 apt-get update
@@ -27,14 +27,14 @@ apt-get install -y bridge-utils build-essential git gnutls-bin libvirt-bin \
 apt-get autoremove -y
 apt-get autoclean
 
-# Define variable for PHP executable
-export php="`which php`"
-
 # Search for PHP executable
-if [ "${php}" = "" ]; then
+if [ "`which php`" = "" ]; then
   echo "Could not find PHP executable."
   exit 2
 fi
+
+# Define variable for PHP executable
+export php="`which php`"
 
 # Make sure required directories are created
 for i in "${confdphp}" `basename "${koalad}"` "${tmp}"; do
@@ -47,14 +47,14 @@ pecl upgrade
 
 # Install gnupg PECL extension
 pecl install gnupg
-echo "extension=gnupg.so" > "${confdphp}/30-gnupg.ini"
+echo "extension=gnupg.so" > "${confdphp}"/30-gnupg.ini
 
 # Build and install libvirt-php
-wget -O "${tmp}/libvirt-php.tar.gz" "${libvirtphp}"
-tar xvf "${tmp}/libvirt-php.tar.gz" -C "${tmp}"
-cd "${tmp}/libvirt-php*"; ./configure; make install; cd "${wd}"
-rm -rf "${tmp}/libvirt-php*"
-echo "extension=libvirt-php.so" > "${confdphp}/30-libvirtphp.ini"
+wget -O "${tmp}"/libvirt-php.tar.gz ${libvirtphp}
+tar xvf "${tmp}"/libvirt-php.tar.gz -C "${tmp}"
+cd "${tmp}"/libvirt-php*; ./configure; make install; cd "${wd}"
+rm -rf "${tmp}"/libvirt-php*
+echo "extension=libvirt-php.so" > "${confdphp}"/30-libvirtphp.ini
 
 # Upgrade koalad if it exists, otherwise clone it to "${koalad}"
 if [ ! -d "${koalad}" ]; then
@@ -64,9 +64,9 @@ cd "${koalad}"; git pull; git submodule update --init
 
 # Setup a reboot cronjob to start koalad
 echo "@reboot root ${php} ${koalad}/main.php 0 > ${logdir}/koalad" > \
-  "${crond}/koalad"
-chmod 644 "${crond}/koalad"
-chown root:root "${crond}/koalad"
+  "${crond}"/koalad
+chmod 644 "${crond}"/koalad
+chown root:root "${crond}"/koalad
 
 echo
 echo "#########################################################################"
@@ -76,12 +76,12 @@ echo "#########################################################################"
 echo
 
 # Check existence of GPG public key
-mkdir -p "${koalad}/data/KoalaCore"
+mkdir -p "${koalad}"/data/KoalaCore
 export gpgpub="${koalad}/data/KoalaCore/gpg.pub"
 touch ${gpgpub}
 if [ "`wc -c \"${gpgpub}\" | awk '{print $1}'`" = "0" ]; then
   echo "Don't forget to install your master's GPG public key at:"
-  echo "${koalad}/data/KoalaCore/gpg.pub"
+  echo "${koalad}"/data/KoalaCore/gpg.pub
 fi
 
 # Alert the user of a required reboot
