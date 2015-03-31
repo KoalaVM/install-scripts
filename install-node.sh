944674ba679fdd743169cc9895fc506f08f748cc
Clay Freeman <git@clayfreeman.com>
2015-03-30 19:30:48 -0500

    Revert "Optimize test checks."
    This reverts commit a74c3afb6fb04c23940d0bf0901d37de54160970.

--- install-node.sh
+++ install-node.sh
@@ -29,9 +29,8 @@ apt-get autoremove -y
 apt-get autoclean
 
 # Test installed packages
-export aptcache="`apt-cache pkgnames`"
 for i in ${packages}; do
-  if [ "`echo \"${aptcache}\" | grep \"${i}\"`" = "" ]; then
+  if [ "`apt-cache pkgnames | grep \"${i}\"`" = "" ]; then
     echo "Package \"${i}\" was not found; exiting."
     exit 2
   fi
@@ -67,9 +66,8 @@ rm -rf "${tmp}"/libvirt-php*
 echo "extension=libvirt-php.so" > "${confdphp}"/30-libvirtphp.ini
 
 # Test PHP modules
-export phpm="`\"${php}\" -m`"
 for i in gnupg libvirt; do
-  if [ "`echo \"${phpm}\" | grep \"${i}\"`" = "" ]; then
+  if [ "`\"${php}\" -m | grep \"${i}\"`" = "" ]; then
     echo "Could not find PHP module \"${i}\"; exiting."
     exit 4
   fi
